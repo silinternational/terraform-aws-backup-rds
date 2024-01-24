86cd40e674a146a243ff52951d9979e927bbd835
briskt <3172830+briskt@users.noreply.github.com>
2024-01-24 09:08:16 -0700

    init the test directory (not the module directory)

--- test/test.tf
+++ test/test.tf
@@ -7,13 +7,13 @@ module "minimal" {
 }
 
 module "full" {
-  source               = "../"
-  app_name             = "full"
-  app_env              = "test"
-  source_arns          = ["arn:aws:rds:us-east-1:123456789012:db:my-db"]
-  backup_cron_schedule = "cron(11 1 * * ? *)"
-  notification_events  = ["BACKUP_JOB_STARTED", "BACKUP_JOB_COMPLETED", "BACKUP_JOB_FAILED", "RESTORE_JOB_COMPLETED"]
-  sns_topic_arn        = "arn:aws:sns:us-east-1:123456789012:backup-vault-events"
+  source              = "../"
+  app_name            = "full"
+  app_env             = "test"
+  source_arns         = ["arn:aws:rds:us-east-1:123456789012:db:my-db"]
+  backup_schedule     = "cron(11 1 * * ? *)"
+  notification_events = ["BACKUP_JOB_STARTED", "BACKUP_JOB_COMPLETED", "BACKUP_JOB_FAILED", "RESTORE_JOB_COMPLETED"]
+  sns_topic_arn       = "arn:aws:sns:us-east-1:123456789012:backup-vault-events"
 }
 
 provider "aws" {
